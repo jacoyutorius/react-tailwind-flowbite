9ba0e77f52e9ace6e0482eba53adda882875a1fd
jacoyutorius <jacoyutorius@gmail.com>
2023-05-12 10:08:03 +0900

    testrun playwright test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,25 @@
-import { test, expect } from '@playwright/test';
+const { test } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('visual regression test', async ({ page }) => {
+  await page.goto('https://playwright.dev/', { waitUntil: 'networkidle' });
+  await page.screenshot({ path: '__screenshots__/screenshot.png', fullPage: true });
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// import { test, expect } from '@playwright/test';
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects the URL to contain intro.
+//   await expect(page).toHaveURL(/.*intro/);
+// });
