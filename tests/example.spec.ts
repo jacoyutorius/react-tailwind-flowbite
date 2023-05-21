05d71462f272fa0949675abe3e7b1a781f6401fe
jacoyutorius <jacoyutorius@gmail.com>
2023-05-22 04:13:19 +0900

    __screenshots__はリポジトリから除外

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,14 @@
+import { useScreenshot } from '../src/hooks/useScreenshot';
+
 const { test } = require('@playwright/test');
 
-test('visual regression test', async ({ page }) => {
+test('visual regression test', async ({ page, context }) => {
   await page.goto('http://127.0.0.1:3000', { waitUntil: 'networkidle' });
+
+  // ブラウザ毎にスナップショットを撮る
+  const { screenShot } = useScreenshot(page, context);
+  await screenShot('estimate.test.png');
+  
   await page.screenshot({ path: '__screenshots__/screenshot.png', fullPage: true });
 });
 
