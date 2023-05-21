211147c8304bb1b733635b4ea461440ea8faaec0
jacoyutorius <jacoyutorius@gmail.com>
2023-05-22 04:20:23 +0900

    minorfix

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,26 +7,5 @@ test('visual regression test', async ({ page, context }) => {
 
   // ブラウザ毎にスナップショットを撮る
   const { screenShot } = useScreenshot(page, context);
-  await screenShot('estimate.test.png');
-  
-  await page.screenshot({ path: '__screenshots__/screenshot.png', fullPage: true });
+  await screenShot('home.png');
 });
-
-// import { test, expect } from '@playwright/test';
-
-// test('has title', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Playwright/);
-// });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects the URL to contain intro.
-//   await expect(page).toHaveURL(/.*intro/);
-// });
